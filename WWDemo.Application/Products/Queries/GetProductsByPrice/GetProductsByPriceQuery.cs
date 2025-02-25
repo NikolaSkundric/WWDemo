fcf8a5d31777bcd4ed4e338e49755ea5c63b1814
NikolaSkundric <skundric.nikola@gmail.com>
2025-02-25 14:52:39 +0100

    Added implementation for GetProductsByPrice endpoint and getProductsByPrice handler and query

---
+++ WWDemo.Application/Products/Queries/GetProductsByPrice/GetProductsByPriceQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WWDemo.Application.DTOs;
+
+namespace WWDemo.Application.Products.Queries.GetProductsByPrice
+{
+    public class GetProductsByPriceQuery : IRequest<List<ProductRepresentation>>
+    {
+        public string? Price { get; set; }
+    }
+}
